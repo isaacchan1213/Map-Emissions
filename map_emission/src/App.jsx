a717634ee185a7519bad1fbb7b4b5838e44eae54
Isaac Chan <isaacchan1213@gmail.com>
2024-07-10 19:38:22 -0400

    changed api google map call

--- map_emission/src/App.jsx
+++ map_emission/src/App.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import {APIProvider, Map} from '@vis.gl/react-google-maps';
+import { useJsApiLoader, GoogleMap } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100vw',
   height: '100vw'
 };
 
-const App = () => (
-  <APIProvider apiKey={import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY}>
-    <Map
-      style={containerStyle}
-      defaultCenter={{lat: 42.3500635818502, lng: -71.10313354361499}}
-      defaultZoom={18}
-      gestureHandling={'greedy'}
-      disableDefaultUI={true}
-    />
-  </APIProvider>
-);
+const center = { lat: 42.3500635818502, lng: -71.10313354361499}
+
+function App() {
+  const {isLoaded} = useJsApiLoader({
+    googleMapsApiKey: import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY
+  })
+
+  if (!isLoaded) {
+    return <h1>loading...</h1>
+  }
+  return (
+    <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={15}
+      >
+    </GoogleMap>
+  );
+}
 
 export default App;
